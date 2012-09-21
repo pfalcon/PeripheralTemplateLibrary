0b020d33f0b28f7444d40ac7af6735b98d1d5305
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2012-09-21 22:09:21 +0300

    Very rought CPU class for MSP430.

---
+++ cpu_msp430.hpp
@@ -0,0 +1,20 @@
+#include <cpu_base.hpp>
+
+class MSP430CPU {};
+
+template <>
+class CPU<MSP430CPU> : public ICPU
+{
+public:
+    static void init(int flags)
+    {
+        switch (flags) {
+        case DEFAULT:
+            // Disable watchdog
+            WDTCTL = WDTPW | WDTHOLD;
+            break;
+        }
+    }
+};
+
+typedef CPU<MSP430CPU> cpu;
