56a916cdeea7b3959b50f1c6ab4cdd1f603615a4
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2012-09-22 01:00:01 +0300

    Add delay() implementation for AVR.

--- delay.hpp
+++ delay.hpp
@@ -1,5 +1,8 @@
 #if defined(__MSP430__)
 #define delay(x) __delay_cycles(x)
+#elif defined(__AVR__)
+#include <util/delay_basic.h>
+#define delay(x) _delay_loop_2(x / 4)
 #else
 #error Unknown platform in delay.hpp
 #endif
