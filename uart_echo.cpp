d3010629a38b2a6fd531f7e2cb275a0ac05f1d82
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2012-09-25 23:52:15 +0300

    Add example of UART echo.

---
+++ uart_echo.cpp
@@ -0,0 +1,20 @@
+#include <gpio.hpp>
+#include <delay.hpp>
+#include <cpu.hpp>
+#include <board.hpp>
+#include <timer.hpp>
+#include <uart.hpp>
+
+typedef UART<Pin<P1, Bit1>, Pin<P1, Bit2>, timer, 1 MHZ / 9600> uart;
+
+int main()
+{
+    cpu::init(cpu::DEFAULT);
+    timer::free_run();
+    uart::init();
+
+    while (true) {
+        uint8_t b = uart::read();
+        uart::write(b);
+    }
+}
