898c3ffd044101f38ad028d96ee7bc83a2bfa054
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2012-09-26 20:44:36 +0300

    Implement pull up/down support.

--- gpio_msp430.hpp
+++ gpio_msp430.hpp
@@ -5,18 +5,19 @@
 #include <msp430.h>
 
 
-template <int in_reg_, int out_reg_, int dir_reg_, typename width_>
+template <int in_reg_, int out_reg_, int dir_reg_, int resistor_reg_, typename width_>
 class Port : public IPort
 {
 public:
     static const int in_reg = in_reg_;
     static const int out_reg = out_reg_;
     static const int dir_reg = dir_reg_;
+    static const int resistor_reg = resistor_reg_;
     typedef width_ width;
 };
 
-typedef Port<P1IN_, P1OUT_, P1DIR_, uint8_t> P1;
-typedef Port<P2IN_, P2OUT_, P2DIR_, uint8_t> P2;
+typedef Port<P1IN_, P1OUT_, P1DIR_, P1REN_, uint8_t> P1;
+typedef Port<P2IN_, P2OUT_, P2DIR_, P2REN_, uint8_t> P2;
 
 template <class port_, class bit>
 class Pin : IPin
@@ -24,7 +25,7 @@ class Pin : IPin
 public:
     typedef port_ port;
 
-    static uint8_t value()
+    static typename port::width value()
     {
         return *(volatile uint8_t*)port::in_reg & bit::value;
     }
@@ -43,6 +44,20 @@ public:
     static void input()
     {
         *(volatile uint8_t*)port::dir_reg &= ~bit::value;
+    }
+    static void pulloff()
+    {
+        *(volatile uint8_t*)port::resistor_reg &= ~bit::value;
+    }
+    static void pullup()
+    {
+        *(volatile uint8_t*)port::resistor_reg |= bit::value;
+        *(volatile uint8_t*)port::out_reg |= bit::value;
+    }
+    static void pulldown()
+    {
+        *(volatile uint8_t*)port::resistor_reg |= bit::value;
+        *(volatile uint8_t*)port::out_reg &= ~bit::value;
     }
 };
 
