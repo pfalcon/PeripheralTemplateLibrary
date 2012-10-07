aac51deee30f88ec69d47c780ce1645b62963136
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2012-10-08 00:35:22 +0300

    Add "Signal" class - abstraction over Pin.
    Unlike Pin which works with high and low logic levels, Signal works
    with asserted and deasserted states, handling signal inversion internally.

---
+++ include/signal.hpp
@@ -0,0 +1,45 @@
+/*
+ * This file is part of the Peripheral Template Library project.
+ *
+ * Copyright (c) 2012 Paul Sokolovsky <pfalcon@users.sourceforge.net>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _SIGNAL_HPP
+#define _SIGNAL_HPP
+
+template <class pin>
+class Signal
+{
+public:
+    static void output()   { pin::output(); }
+    static void input()    { pin::input(); }
+    static void assert()   { pin::high(); }
+    static void deassert() { pin::low(); }
+    static bool value()    { return pin::value(); }
+};
+
+template <class pin>
+class InvertedSignal
+{
+public:
+    static void output()   { pin::output(); }
+    static void input()    { pin::input(); }
+    static void assert()   { pin::low(); }
+    static void deassert() { pin::high(); }
+    static bool value()    { return !pin::value(); }
+};
+
+#endif //_SIGNAL_HPP
+
