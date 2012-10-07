ca1a253ed80c35d5ad3282a83cd0610fd2838166
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2012-10-07 22:09:01 +0300

    Add ADC example (MSP430 specific).

---
+++ adc.cpp
@@ -0,0 +1,28 @@
+#include <cpu.hpp>
+#include <board.hpp>
+#include <gpio.hpp>
+#include <delay.hpp>
+#include <timer.hpp>
+#include <uart.hpp>
+#include <console.hpp>
+#include <adc.hpp>
+
+
+typedef UART<Pin<P1, Bit1>, Pin<P1, Bit2>, timer, 1 MHZ / 9600> uart;
+typedef Console<uart> con;
+typedef ADC adc;
+
+int main()
+{
+    cpu::init(cpu::DEFAULT);
+    timer::free_run();
+    uart::init();
+    adc::unlock();
+    adc::config<ADCDefaultConfig>();
+    while (1) {
+        uart::read();
+        adc::width value = adc::easy_sample(adc::VCC_HALF);
+        con::putdec(value);
+        con::newline();
+    }
+}
