71de30709a3e1603e0b328642b25b867f5f5eaec
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2012-10-20 00:15:32 +0300

    Remove deprecated header.

--- include/delay_static.hpp
+++ include/delay_static.hpp
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include <delay_static_base.hpp>
 #if defined(__MSP430__)
 #include <delay_static_msp430.hpp>
 #elif defined(__AVR__)
