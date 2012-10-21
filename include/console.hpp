5cb9a626e7cf7358e73c438e303ada7fb77d0de3
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2012-10-21 23:43:58 +0300

    Add ENDL constant string definition for UART.

--- include/console.hpp
+++ include/console.hpp
@@ -16,6 +16,9 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+#define ENDL "\r\n"
+
 template <class byte_writer>
 class Console
 {
