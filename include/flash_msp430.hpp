a3296d9a3be2a97e4cf593d67966c3307d46112e
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2012-11-17 11:12:02 +0200

    Draft Flash ROM access header for MSP430.

---
+++ include/flash_msp430.hpp
@@ -0,0 +1,62 @@
+/*
+ * This file is part of the Peripheral Template Library project.
+ *
+ * Copyright (c) 2012 Paul Sokolovsky <pfalcon@users.sourceforge.net>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// 1MHz / 3 = 333KHz
+#define FLASH_CLOCK_DIV 3
+
+class Flash
+{
+public:
+        static void init()
+        {
+            // MCLK should be 1MHz
+            FCTL2 = FWKEY | FSSEL_1 | (FLASH_CLOCK_DIV - 1);
+        }
+
+        static void erase(void *block)
+        {
+            // Watchdog should be disabled
+            // Reset LOCK and other status bits
+            FCTL3 = FWKEY;
+            FCTL1 = FWKEY | ERASE;
+            *(uint8_t*)block = 0;
+            FCTL1 = FWKEY;
+            FCTL3 = FWKEY | LOCK;
+        }
+
+        static void write_u8(uint8_t *addr, uint8_t val)
+        {
+            // Reset LOCK and other status bits
+            FCTL3 = FWKEY;
+            FCTL1 = FWKEY | WRT;
+            *addr = val;
+            FCTL1 = FWKEY;
+            FCTL3 = FWKEY | LOCK;
+        }
+
+        static void write_u16(uint16_t *addr, uint16_t val)
+        {
+            // Reset LOCK and other status bits
+            FCTL3 = FWKEY;
+            FCTL1 = FWKEY | WRT;
+            *addr = val;
+            FCTL1 = FWKEY;
+            FCTL3 = FWKEY | LOCK;
+        }
+};
