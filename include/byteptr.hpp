dda1ad1ec3e9725b78d6f95c166f72252020682e
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2012-11-29 20:33:20 +0200

    Add unit16_t setter.

--- include/byteptr.hpp
+++ include/byteptr.hpp
@@ -4,4 +4,5 @@ class byteptr
 {
 public:
     static uint16_t _uint16_t(void *p) { return (((uint8_t*)p)[1] << 8) | *(uint8_t*)p; }
+    static void _uint16_t(void *p, uint16_t val) { *(uint8_t*)p = val; ((uint8_t*)p)[1] = val >> 8; }
 };
