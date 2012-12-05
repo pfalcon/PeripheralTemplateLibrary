e7d6a5a96a9ebac1ba23f0dfd92293696c882812
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2012-12-05 22:40:16 +0200

    Add uint32_t support and rewrite for, well, clarity.

--- include/byteptr.hpp
+++ include/byteptr.hpp
@@ -3,6 +3,31 @@
 class byteptr
 {
 public:
-    static uint16_t _uint16_t(void *p) { return (((uint8_t*)p)[1] << 8) | *(uint8_t*)p; }
-    static void _uint16_t(void *p, uint16_t val) { *(uint8_t*)p = val; ((uint8_t*)p)[1] = val >> 8; }
+    static uint16_t _uint16_t(const void *p)
+    {
+        const uint8_t *t = (uint8_t*)p;
+        return (t[1] << 8) | t[0];
+    }
+
+    static void _uint16_t(void *p, uint16_t val)
+    {
+        uint8_t *t = (uint8_t*)p;
+        t[0] = val;
+        t[1] = val >> 8;
+    }
+
+    static uint32_t _uint32_t(const void *p)
+    {
+        const uint8_t *t = (uint8_t*)p;
+        return ((uint32_t)t[3] << 24) | ((uint32_t)t[2] << 16) | (t[1] << 8) | t[0];
+    }
+
+    static void _uint32_t(void *p, uint32_t val)
+    {
+        uint8_t *t = (uint8_t*)p;
+        t[0] = val;
+        t[1] = val >> 8;
+        t[2] = val >> 16;
+        t[3] = val >> 24;
+    }
 };
