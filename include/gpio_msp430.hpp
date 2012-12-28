935b4bf97045d922cc3d40c3870a61be81f73362
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2012-12-28 21:54:31 +0200

    Add Port 3.

--- include/gpio_msp430.hpp
+++ include/gpio_msp430.hpp
@@ -36,6 +36,7 @@ public:
 
 typedef Port<P1IN_, P1OUT_, P1DIR_, P1REN_, uint8_t> P1;
 typedef Port<P2IN_, P2OUT_, P2DIR_, P2REN_, uint8_t> P2;
+typedef Port<P3IN_, P3OUT_, P3DIR_, P3REN_, uint8_t> P3;
 
 template <class port_, class bit>
 class Pin : IPin< Pin<port_, bit> >
