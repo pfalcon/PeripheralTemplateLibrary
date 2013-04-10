1d305439b50503aca85f248dee4ec3667bab5787
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2013-04-11 01:30:38 +0300

    For help with debugging, blink LED too.
    Also, use Arduino connection on single port.

--- lcd.cpp
+++ lcd.cpp
@@ -10,14 +10,16 @@
 
 typedef TimeDelay<board::freq, StaticDelay> delayer;
 typedef ParBus<board::D5, board::D4, board::D3, board::D2> lcd_data;
-typedef Signal<board::D11> en_sig;
-typedef Signal<board::D12> regsel_sig;
+typedef Signal<board::D6> en_sig;
+typedef Signal<board::D7> regsel_sig;
 typedef HD44780<delayer, lcd_data, en_sig, regsel_sig> lcd;
 typedef Console<lcd> lcd_con;
 
 int main()
 {
     cpu::init(cpu::DEFAULT);
+    board::LED::port::enable();
+    board::LED::output();
     lcd::init();
     lcd_con::putstr("Hello, World!");
     int c = 0;
@@ -25,5 +27,6 @@ int main()
         lcd::set_pos(0, 1);
         lcd_con::putdec(c++);
         delayer::delay_ms(1000);
+        board::LED::toggle();
     }
 }
