0221ff2821afc6e6c9733e31a716ea4a23cb2ebd
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2013-05-02 20:53:22 +0300

    Add hd44780 bus config for msp430.

--- examples/hw_config_msp430.hpp
+++ examples/hw_config_msp430.hpp
@@ -1,2 +1,10 @@
+#include <signal.hpp>
+#include <parallel_bus.hpp>
+#include <uart.hpp>
+
 //typedef UART<board::freq, 9600, board::pin_uart_tx, board::pin_uart_rx, timer> uart;
 typedef UART<1 MHZ, 9600, USCI> uart;
+
+typedef ParBus< Pin<P1, Bit5>, Pin<P1, Bit4>, Pin<P1, Bit3>, Pin<P1, Bit2> > hd44780_data;
+typedef Signal< Pin<P1, Bit6> > hd44780_en_sig;
+typedef Signal< Pin<P1, Bit7> > hd44780_regsel_sig;
