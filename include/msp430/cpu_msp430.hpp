28c9318cfc2a6557383a90b1823868ec2b541439
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2013-05-05 16:27:41 +0300

    Elaborate MSP430 blocks definitions, for generalized IRQ routing support.

--- include/msp430/cpu_msp430.hpp
+++ include/msp430/cpu_msp430.hpp
@@ -26,7 +26,37 @@
 class MSP430CPU {};
 
 // Various blocks of CPU
-class USCI {};
+
+enum { MSP430_NONE, MSP430_TIMER, MSP430_USART, MSP430_USI, MSP430_USCI };
+
+// All implementation of MSP430 blocks support should inherit from this class.
+class MSP430Block
+{
+public:
+    const static int block_type = MSP430_NONE;
+};
+
+/*
+ * MSP430 serial communications implementation is pretty twisted thing.
+ * Let's start with saying that MSP430 series has 3 different serial
+ * comminication controllers: USART, USI, USCI. So, below we define
+ * empty ID classes to specialize protocol implementation against.
+ */
+
+// USART is contoller in "original" MSP430 series, doesn't seem to be
+// available in "value line".
+struct USART {};
+// USI & USCI are "value line" novelties.
+// USI doesn't support UART and otherwise "lower profiler" than USCI.
+struct USI {};
+// USCI consists of two blocks: USCI_A & USCI_B. They appear to always
+// come together, i.e. there doesn't seem to be a chip model with only
+// USCI_A or USCI_B (though, with TI engineers and marketologists, you
+// can only keep fingers crossed). USCI_A capable of doing UART and SPI,
+// USCI_B - SPI and I2C.
+struct USCI_A { const static int no = 'A'; };
+struct USCI_B { const static int no = 'B'; };
+
 
 template <>
 class CPU<MSP430CPU> : public ICPU
