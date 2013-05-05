45b56c59d433e8dc6761506ead2cc2d75ae5e685
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2013-05-05 16:36:40 +0300

    Add hardware SPI definition.

--- examples/hw_config_msp430.hpp
+++ examples/hw_config_msp430.hpp
@@ -5,6 +5,8 @@
 //typedef UART<board::freq, 9600, board::pin_uart_tx, board::pin_uart_rx, timer> uart;
 typedef UART<1 MHZ, 9600, USCI_A> uart;
 
+typedef SPI<USCI_B> spi;
+
 typedef ParBus< Pin<P1, Bit5>, Pin<P1, Bit4>, Pin<P1, Bit3>, Pin<P1, Bit2> > hd44780_data;
 typedef Signal< Pin<P1, Bit6> > hd44780_en_sig;
 typedef Signal< Pin<P1, Bit7> > hd44780_regsel_sig;
