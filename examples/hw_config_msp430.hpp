0928234a1d23bd41a7f1e44a63d670e7b6d0bc24
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-08-14 11:14:02 +0300

    examples: HW configs: explicitly define hardware uart object (hwuart).
    Some examples require hardware UART with interrupt support, etc., so make
    it obvious.

--- examples/hw_config_msp430.hpp
+++ examples/hw_config_msp430.hpp
@@ -5,6 +5,10 @@
 
 using namespace PTL;
 
+//#define LAUNCHPAD_MSP430_VER_1_4
+
+#ifndef LAUNCHPAD_MSP430_VER_1_4
+
 /* MSP430 Launchpad hw. rev. 1.5 has different (swapped) pin assignments for
    "software" and "hardware" UART. This is rooted in the fact that hwrev. 1.4
    had "software" assignment, but later released chips with hardware UART
@@ -18,17 +22,22 @@ using namespace PTL;
    1.4 support.
    More info: http://pfalcon-oe.blogspot.com/2012/07/ti-launchpad-14-vs-15-pin-swappery.html
    */
+typedef UART<1 MHZ, 9600, USCI_A> hwuart;
 //typedef UART<board::freq, 9600, board::pin_uart_tx, board::pin_uart_rx, timer> uart;
 typedef UART<1 MHZ, 9600, USCI_A> uart;
 
-/* MSP430 Launchpad hw. rev. 1.4 doesn't have hardware UART support at all,
-   and bitbang UART has to use swapped RXD/TXD pins. */
-//typedef UART<board::freq, 9600, Pin<P1, Bit1>, Pin<P1, Bit2>, timer> uart;
-
-/* At this time, only USCI-based hardware SPI is supported, USI is still needs
+/* At this time, only USCI-based hardware SPI is supported, USI still needs
    wrapping. Note that USCI vs USI SPI pins have the same swapping issue as UART
    above (great work, TI!) */
 typedef SPI<USCI_B> spi;
+
+#else
+
+/* MSP430 Launchpad hw. rev. 1.4 doesn't have hardware UART support at all,
+   and bitbang UART has to use swapped RXD/TXD pins. */
+typedef UART<board::freq, 9600, Pin<P1, Bit1>, Pin<P1, Bit2>, timer> uart;
+
+#endif
 
 typedef ParBus< Pin<P1, Bit5>, Pin<P1, Bit4>, Pin<P1, Bit3>, Pin<P1, Bit2> > hd44780_data;
 typedef Signal< Pin<P1, Bit6> > hd44780_en_sig;
